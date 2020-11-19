9e7c0cf1eb2cab1d93c9c41c566bb7c71faaeec8
Nathan Polomark <polomarkn@hotmail.com>
2020-11-18 22:07:25 -0800

    Changed table set up and added more fields

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,34 +1,20 @@
 import React from "react";
 
-function UserDetails(props) {
-  return (
-    <div className="text-center">
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Firstname</th>
-            <th>Location</th> 
-            <th>Email</th>
-          </tr>
-        </thead>
+function Table(props) {
+    return (
+
         <tbody>
-          <tr>
-            <td>{props.name}</td>
-            <td>{props.location}</td> 
-            <td>{props.email}</td>
-          </tr>
+            <tr>
+                <td><img src={props.image} alt={props.first}></img></td>
+                <td>{props.first}</td>
+                <td>{props.last}</td>
+                <td>{props.email}</td>
+                <td>{props.age}</td>
+                <td>{props.city}</td>
+                <td>{props.state}</td>
+            </tr>
         </tbody>
-      </table>
 
-
-
-      {/* <img alt={props.title} className="img-fluid" src={props.src} style={{ margin: "0 auto" }} />
-      <h3>Director(s): {props.director}</h3>
-      <h3>Genre: {props.genre}</h3>
-      <h3>Released: {props.released}</h3> */}
-    </div>
-  );
+    )
 }
-
-export default UserDetails;
+export default Table;
